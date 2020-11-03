509b14ba6bec09b1ba3d55c00c1ef60e4b78a38e
gkinghan <849414545@qq.com>
2020-11-03 12:38:22 +0800

    timeStamp参数调整

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
 import store from '@/store/index.js'
+import JSONbig from 'json-bigint'
 // 基于axios扩展配置
 const http = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn'
+  baseURL: 'http://ttapi.research.itcast.cn',
+  // 大数转换处理
+  transformResponse: [function (data) {
+    // data => 原始的后台返回的 json 字符串数据
+    // 这里的方法, 会覆盖axios默认对于返回结果的处理, 默认进行的是 JSON.parse
+    // 需要进行的是 JSONbig.parse
+
+    // 后台返回的有可能不是 json字符串, JSONbig 解析就会报错, 不合理的, 需要处理
+    try {
+      return JSONbig.parse(data)
+    } catch (err) {
+      // 如果解析不了, 说明不是json格式, 原样输出
+      return data
+    }
+  }]
 
 })
 
--- src/views/home/articleList.vue
+++ src/views/home/articleList.vue
@@ -6,7 +6,7 @@
         finished-text="没有更多了"
         @load="onLoad"
         >
-        <van-cell v-for="item in list" :key="item.art_id" :title="item.title" />
+        <van-cell v-for="item in list" :key="item.art_id.toString()" :title="item.title" />
     </van-list>
   </div>
 </template>
@@ -29,8 +29,13 @@ export default {
   methods: {
     // 1 在一进入页面发现数据不够会触发。2 滑动到底部时自动触发
     async onLoad () {
+      if (!this.timestamp) {
+        this.timestamp = +new Date()
+      }
       // 1 发送请求获取数据
-      const res = await reqGetArticles(this.channel.id, +new Date())
+      const res = await reqGetArticles(this.channel.id, this.timestamp)
+      // 更新时间戳
+      this.timestamp = res.data.data.pre_timestamp
       // 2 将数据追加
       const arr = res.data.data.results
       this.list = [...this.list, ...arr]
